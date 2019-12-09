26bed40ff8dc6f8dbc83f9fbd83f08bced4562fb
Guobang Bi <373318510@qq.com>
2019-12-09 17:40:26 +0800

    complete test flaskr

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -29,10 +29,77 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
+
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data)
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertLessEqual(len(data['questions']), 10)
+        self.assertTrue(data)
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/2')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data)
+        self.assertTrue(data['success'])
+
+    def test_create_question(self):
+        res = self.client().post('/questions', json={'question': 'Who was the first president of the USA?',
+                                                     'answer': 'George Washington',
+                                                     'category': 4,
+                                                     'difficulty': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data)
+        self.assertTrue(data['success'])
+    
+    def test_search_question(self):
+        res = self.client().post('/questions', json={'searchTerm': 'boxer'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data)
+        self.assertLessEqual(len(data['questions']), 10)
+        self.assertIn('boxer', data['questions'][0]['question'])
+
+    def test_get_questions_based_on_category(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data)
+        self.assertLessEqual(len(data['questions']), 10)
+
+    def test_404_if_questions_does_not_exist(self):
+        res = self.client().get('/categories/1000/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data['success'])
+        self.assertEqual(data['message'], "The requested resource doesn't exist.")
+
+    def test_405_if_methods_not_allowed(self):
+        res = self.client().get('questions/5')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertFalse(data['success'])
+        self.assertEqual(data['message'], 'method not allowed')
 
 
 # Make the tests conveniently executable
